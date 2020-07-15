d734cdd985e37aca790044cf29469735d78b6c32
kollarmark11 <kollarmark11@gmail.com>
2020-07-15 11:22:27 +0200

    vuetify starter

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Home from './Home.vue';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
-Vue.component('app-server-status', Home);
+Vue.config.productionTip = false;
 
 new Vue({
-  el: '#app',
+  router,
+  store,
+  vuetify,
   render: h => h(App)
-})
+}).$mount("#app");
